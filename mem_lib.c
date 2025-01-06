a7d2b98e5ba86f397857e333f80448dd62df4ce1
kamstand <kamrul.hassan@stonybrook.edu>
2025-01-06 17:11:00 -0500

    Created new HEAP_CHUNK struct and initialized heap

--- mem_lib.c
+++ mem_lib.c
@@ -1,1 +1,50 @@
+#include "mem_lib.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+static HEAP_CHUNK *HEAD = NULL; // Head of the linked list
+static char *HEAP = NULL;  // Pointer to the start of the heap
+static size_t HEAP_SIZE = 1024 * 1024; // 1 MB heap size (adjustable)
+static void *HEAP_START_ADDR; // starting address of heap
+static void *HEAP_END_ADDR; // ending address of heap
+
+void HEAP_INIT(){
+
+    // get the starting address(current program break)
+    HEAP_START_ADDR = sbrk(0);
+    
+    void *increment_heap = sbrk(HEAP_SIZE); // increment by 1MB
+
+    if (increment_heap == (void *)-1) {
+        perror("SBRK did nto work");
+        return;
+    }
+    // set the ending address of heap to the new program break
+    HEAP_END_ADDR = increment_heap;
+
+    //Initialize the first block
+    HEAP_CHUNK *first = (HEAP_CHUNK *)HEAP_START_ADDR; // set to the starting address of heap
+    first->size = HEAP_SIZE - sizeof(HEAP_CHUNK); // set the size of the first free 
+    first->isFree = 1; // mark as free
+    first->next = NULL; // next is null
+    first->prev = NULL; // prev is null
+    first->buff =((char *)HEAP_START_ADDR + sizeof(HEAP_CHUNK)); // this is where the data starts
+
+    if (HEAD == NULL) // must check if head is null because then we will assign it as such
+    {
+        HEAD = first;
+        return;
+    }
+
+    return;
+
+}
+
+void *HEAP_START(){
+    return HEAP_START_ADDR;
+}
+
+void *HEAP_END(){
+    return HEAP_END_ADDR;
+}
--- mem_lib.h
+++ mem_lib.h
@@ -9,8 +9,29 @@ typedef struct HEAP_CHUNK {
     struct HEAP_CHUNK *next; 
     struct HEAP_CHUNK *prev; 
     int isFree;
-    char *start;
+    char *buff;
 } HEAP_CHUNK;
 
+/**
+ * @brief Returns the starting address of the heap 
+ * 
+ * @return void* 
+ */
+void *HEAP_START();
+
+/**
+ * @brief Returns the ending address of the heap
+ * 
+ * @return void* 
+ */
+void *HEAP_END();
+
+/**
+ * @brief initializes the heap using sbrk() to exactly 1MB
+ * 
+ * @return void
+ */
+
+void HEAP_INIT();
 
 #endif // MEM_LIB_H
